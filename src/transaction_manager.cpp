db0263fc4b84203f95d1e8cee9b3067e81e24d5b
SiddhantMeshram <sidmeshram97@gmail.com>
2024-11-04 17:04:22 -0500

    Parsing Input.

--- src/transaction_manager.cpp
+++ src/transaction_manager.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <sstream>
+#include <unordered_set>
+#include <cassert>
 
 #include "transaction_manager.h"
 
@@ -19,7 +22,41 @@ void TransactionManager::ProcessInput(const string& file_name) {
   ifstream input(file_name);
   string line;
   while (getline(input, line)) {
-    cout << line << endl;
+    string command, var;
+    unordered_set<char> delimiters{'(', ')', ','};
+    vector<string> variables;
+
+    for (int ii = 0; ii < line.size(); ++ii) {
+        if (delimiters.find(line[ii]) != delimiters.end()) {
+            line[ii] = ' ';
+        }
+    }
+
+    istringstream iss(line);
+    iss >> command;
+    while (iss >> var) {
+      variables.push_back(var);
+    }
+
+    if (command == "begin") {
+
+    } else if (command == "R") {
+
+    } else if (command == "W") {
+
+    } else if (command == "fail") {
+
+    } else if (command == "recover") {
+
+    } else if (command == "end") {
+
+    } else if (command == "fail") {
+
+    } else if (command == "dump") {
+
+    } else {
+      assert("Unexpected command");
+    }
   }
 
   input.close();
